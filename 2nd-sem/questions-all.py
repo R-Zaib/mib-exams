70b89caf242fd1d7bb8602f4842abfb876b8aa5a
Don <don@Dons-Air.fritz.box>
2024-06-05 16:23:19 +0200

    updated

--- 2nd-sem/questions-all.py
+++ 2nd-sem/questions-all.py
@@ -1,6 +1,9 @@
 """
+q1: create a python function that uses a regular expression to return all the substrings that contain at least one letter of english 
+alphabet immediately followed two numbers, out of which the first cannot be zero,  and then finally a space; from a text input.
+
 scrape the wikipedia page for chess. Using for example an Xpath expression, search for the "Rules" section. Create a custom html
 with a templfor the scraped content
 
-
+q2: what are the main differentces between multiprocessing and asyncio? Write a usecase for each
 """
